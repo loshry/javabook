bd6e7db0b3582d15729eddd962a29aef9da2cc77
Leah Oshry <owlinnyc@gmail.com>
2022-11-05 18:53:37 -0400

    updated

---
+++ src/com/company/DrumKit.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare() {
+        System.out.println("bang bang ba-bang");
+    }
+
+}
